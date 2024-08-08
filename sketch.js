f2c776d9e4657989bc3a782708a33c1ef85d06ad
viniciusalbino <vinicius.krieser@escola.pr.gov.br>
2024-08-08 16:35:31 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+    background("black");
+  }
+  
+  function draw() {
+  
+    (30,20,20);
+    fill("purple");
+    stroke("cyan");
+    if(mouseIsPressed);{
+    rect(mouseX,mouseY,20.20);}
+  }
+  
